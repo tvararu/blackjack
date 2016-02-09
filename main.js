98f24bc1c50ba6693179b6f13457e76d1403830d
Theodor Vararu <theo@vararu.org>
2016-02-09 21:36:33 +0000

    Implement getCardNumbers and getDeck.

--- main.js
+++ main.js
@@ -4,6 +4,28 @@
   const util = {
     getSuites () {
       return ['♠', '♥', '♦', '♣']
+    },
+
+    getCardNumbers () {
+      const twoToTen = (Array.from(new Array(9), x => x + 2))
+      return [
+        { name: 'Ace', value: 11 },
+        ...(twoToTen.map(n => ({ name: n, value: n }))),
+        { name: 'Jack', value: 10 },
+        { name: 'Queen', value: 10 },
+        { name: 'King', value: 10 }
+      ]
+    },
+
+    getDeck (suites, cardNumbers) {
+      return suites.reduce((acc, suite) => {
+        const cardsOfSuite = cardNumbers.map(cn => ({
+          name: cn.name,
+          value: cn.value,
+          suite
+        }))
+        return [...acc, ...cardsOfSuite]
+      }, [])
     }
   }
 
--- spec.js
+++ spec.js
@@ -13,3 +13,20 @@ test('util.getSuites returns four suites', (assert) => {
   assert.equal(util.getSuites().length, 4)
   assert.end()
 })
+
+test('util.getCardNumbers return cards Ace to King', (assert) => {
+  const cardNumbers = util.getCardNumbers()
+  assert.equal(cardNumbers.length, 13)
+  const ace = cardNumbers[0]
+  assert.equal(ace.name, 'Ace')
+  assert.equal(ace.value, 11)
+  assert.end()
+})
+
+test('util.getDeck returns 52 cards', (assert) => {
+  const cardNumbers = util.getCardNumbers()
+  const suites = util.getSuites()
+  const deck = util.getDeck(suites, cardNumbers)
+  assert.equal(deck.length, 52)
+  assert.end()
+})
